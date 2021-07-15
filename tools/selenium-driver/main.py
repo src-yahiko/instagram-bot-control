967376fb7258f00954d52e34bf0976d5a254b211
Omar Bittar <omrbtr0@gmail.com>
2021-07-16 00:38:59 +0200

    selenium-driver py

---
+++ tools/selenium-driver/main.py
@@ -0,0 +1,42 @@
+import undetected_chromedriver.v2 as uc
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+import argparse
+
+def parse_arguments():
+	"""Parses the program arguments (fails if required argmuents are not given)"""
+	parser = argparse.ArgumentParser(description="Selenium Driver Generator")
+
+	parser.add_argument(
+		"--proxy",
+		required=False,
+		help="Proxy IP address to use",
+		default=None)
+
+	parser.add_argument(
+		"--profile",
+		required=False,
+		help="Set a browser profile name",
+		default="Default")
+
+	parser.add_argument(
+		"--url",
+		required=False,
+		help="Opens a specified url",
+		default="https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
+
+	return parser.parse_args()
+
+def driver(profile="Default", proxyIP=None):
+	if proxyIP != None:
+		print("Proxy not implemented yet")
+
+	options = uc.ChromeOptions()
+	options.user_data_dir = f"./profiles/{profile}"
+	options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+	driver = uc.Chrome(options=options)
+	return driver
+
+if __name__ == '__main__':
+	args = parse_arguments()
+	driver = driver(args.profile, args.proxy)
+	driver.get(args.url)
