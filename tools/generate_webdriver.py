a86f6a5dc0605e4636dc6b26589f81643d1c15a3
Omar Bittar <omrbtr0@gmail.com>
2021-07-16 14:22:48 +0200

    add headless flag

--- tools/generate_webdriver.py
+++ tools/generate_webdriver.py
@@ -1,6 +1,7 @@
 import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 import argparse
+from time import sleep
 
 def parse_arguments():
 	"""Parses the program arguments (fails if required argmuents are not given)"""
@@ -15,8 +16,14 @@ def parse_arguments():
 	parser.add_argument(
 		"--profile",
 		required=False,
-		help="Set a browser profile name",
+		help="Set a browser profile",
 		default="Default")
+
+	parser.add_argument(
+		"--headless",
+		required=False,
+		help="Set the browser to headless, default false",
+		default=False)
 
 	parser.add_argument(
 		"--url",
@@ -26,11 +33,15 @@ def parse_arguments():
 
 	return parser.parse_args()
 
-def generate(profile="Default", proxyIP=None, url=None):
+def generate(profile="Default", proxyIP=None, url=None, headless=False):
 	if proxyIP != None:
 		print("Proxy not implemented yet")
-
 	options = uc.ChromeOptions()
+	if headless:
+		options.add_argument("--disable-extensions")
+		options.add_argument("--disable-gpu")
+		options.add_argument("--no-sandbox") # linux only
+		options.add_argument("--headless")
 	options.user_data_dir = f"../selenium_profiles/{profile}"
 	options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
 	driver = uc.Chrome(options=options)
@@ -40,5 +51,8 @@ def generate(profile="Default", proxyIP=None, url=None):
 
 if __name__ == '__main__':
 	args = parse_arguments()
-	driver = generate(args.profile, args.proxy)
+	driver = generate(args.profile, args.proxy, headless=args.headless)
 	driver.get(args.url)
+	if args.headless:
+		driver.save_screenshot("_screenshot.png")
+	sleep(60)
